189f5d0a5bab860cf34d3fe52c6479aa67126814
shanshan.chen <shanshan.chen@ptmind.com>
2019-10-23 17:31:20 +0800

    Site updated: 2019-10-23 17:31:20

--- downloads/code/demo.js
+++ downloads/code/demo.js
@@ -2,4 +2,22 @@ const a = 1;
 const b = 2;
 function sort(a, b){
     return a - b;
-}
+}<script>
+        document.querySelectorAll('.github-emoji')
+          .forEach(el => {
+            if (!el.dataset.src) { return; }
+            const img = document.createElement('img');
+            img.style = 'display:none !important;';
+            img.src = el.dataset.src;
+            img.addEventListener('error', () => {
+              img.remove();
+              el.style.color = 'inherit';
+              el.style.backgroundImage = 'none';
+              el.style.background = 'none';
+            });
+            img.addEventListener('load', () => {
+              img.remove();
+            });
+            document.body.appendChild(img);
+          });
+      </script>
